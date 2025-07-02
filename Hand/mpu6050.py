a1a86bf85e36de0a9863b42f8a9c7fe00e811b1b
Shourya Eaga <shourya.eaga.09@gmail.com>
2025-07-02 22:46:23 +0100

    adding configs to accelerometer and gyroscope

--- Hand/mpu6050.py
+++ Hand/mpu6050.py
@@ -2,6 +2,10 @@ from machine import I2C
 from utime import sleep_ms
 _PWR_MGMT_1 = 0x6B
 _GYRO_CONFIG = 0x1B
+_ACCEL_CONFIG = 0x1C
+
+GYRO_FULL_SCALE_RANGE = 250
+ACCEL_FULL_SCALE_RANGE = 2
 
 print("MPU6050 MODULE LOADED")
 
@@ -20,7 +24,30 @@ class MPU6050:
             raise Exception
 
         self.gyro_config()
+        self.accel_config()
         
     def gyro_config(self):
         data = self.i2c.readfrom_mem(self.address, _GYRO_CONFIG, 1)
-        print(data)
+        fs_sel = (data[0] >> 3) & 0b11
+        if fs_sel == 0:
+            GYRO_FULL_SCALE_RANGE = 250
+        elif fs_sel == 1:
+            GYRO_FULL_SCALE_RANGE = 500
+        elif fs_sel == 2:
+            GYRO_FULL_SCALE_RANGE = 1000
+        elif fs_sel == 3:
+            GYRO_FULL_SCALE_RANGE = 2000
+        print(f"Set gyro full scale range to {GYRO_FULL_SCALE_RANGE} degrees /s")
+
+    def accel_config(self):
+        data = self.i2c.readfrom_mem(self.address, _ACCEL_CONFIG, 1)
+        afs_sel = (data[0] >> 3) & 0b11
+        if afs_sel == 0:
+            ACCEL_FULL_SCALE_RANGE = 2
+        elif afs_sel == 1:
+            ACCEL_FULL_SCALE_RANGE = 4
+        elif afs_sel == 2:
+            ACCEL_FULL_SCALE_RANGE = 8
+        elif afs_sel == 3:
+            ACCEL_FULL_SCALE_RANGE = 16
+        print(f"Set accel full scale range to {ACCEL_FULL_SCALE_RANGE} g")
